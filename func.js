6fbfad42094a63b025b6fe8f4e009983ff2de556
Samir Ahajin <samirahajin@gmail.com>
2022-02-02 18:26:26 +0800

    added click show

--- func.js
+++ func.js
@@ -1,10 +1,14 @@
 let gridCon = (function(){
     let _mainGrid = document.getElementById('main-grid')
 
+
     function render(){
         _createGrid();
+
         let images = document.querySelectorAll(".image");
         images.forEach(img=>img.addEventListener("click",_zoomImage));
+
+
 
     }
     function _createGrid(){
@@ -19,8 +23,24 @@ let gridCon = (function(){
             _mainGrid.appendChild(newGrid);
         }
     }
-    function _zoomImage(){
-            console.log("hello");
+    function _zoomImage(event){
+            let backGround = document.getElementById("zoom");
+            let newImage = document.createElement("img");
+            let container = document.createElement("div");
+            
+            
+            newImage.setAttribute("class","zoom-image");
+            newImage.setAttribute("src",`images\/image-${event.target.id}.jpg`);
+            container.appendChild(newImage);
+            backGround.appendChild(container);
+            backGround.style.display = "block";
+
+            backGround.addEventListener("click",()=>{
+                backGround.style.display = "none";
+                backGround.removeChild(container);
+            })
+
+
     }
     
     return{
